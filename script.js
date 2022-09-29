e0f868e80f83e7b11a2b5840e530ffbe39b51b85
L Rakesh Singh <keztmckinnon@gmail.com>
2022-09-29 14:58:02 +0530

    feat update

--- script.js
+++ script.js
@@ -8,22 +8,28 @@ var head=0;
 
 function selectHead(){
     head=head+1;
-    var randomNumber=Math.floor(Math.random()*1)
+    var randomNumber=Math.floor(Math.random()*2)
     console.log(randomNumber)
     console.log(head)
     if (Number(randomNumber)===Number(head)){
         output.innerText=`You have selected head and you win 🚀🚀`
+        head=0;
+        ;
     }else {
         output.innerText=`You have selected head still you didn't win 😭😭`
+        head=0;
     }
 }
 function selectTail(){
-    var randomNumber=Math.floor(Math.random()*1)
+    var randomNumber=Math.floor(Math.random()*2)
     console.log(randomNumber)
     console.log(head)
     if (Number(randomNumber)===Number(head)){
         output.innerText=`You have selected tail and you still win 😇😇`
+        head=0;
+        
     }else {
         output.innerText=`You are the unluckiest person, take a bath please 😭😭`
+        head=0;
     }
 }
